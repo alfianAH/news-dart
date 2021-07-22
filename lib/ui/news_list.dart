f81755186906573fe9ac2c3bbbb1588b0671d367
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-07-22 23:42:51 +0800

    Add scroll controller in news list

--- lib/ui/news_list.dart
+++ lib/ui/news_list.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:news_app/model/news_model.dart';
 import 'package:news_app/ui/news_item.dart';
 
-class NewsList extends StatelessWidget{
-
+class NewsList extends StatefulWidget{
   final NewsModel newsModel;
   final String listTitle;
 
@@ -14,44 +13,59 @@ class NewsList extends StatelessWidget{
   });
 
   @override
+  _NewsListState createState() => _NewsListState();
+}
+
+class _NewsListState extends State<NewsList> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
 
     return SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Title
-              Text(
-                listTitle,
-                style: textTheme.headline2,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
+      controller: _scrollController,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Title
+            Text(
+              widget.listTitle,
+              style: textTheme.headline2,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
 
-              // News list
-              ListView.builder(
-                shrinkWrap: true,
-                itemBuilder: (context, index){
-                  // Check image URL is null or not
-                  Articles newsArticle = newsModel.articles![index]!;
+            // News list
+            ListView.builder(
+              controller: _scrollController,
+              shrinkWrap: true,
+              itemBuilder: (context, index){
+                // Check image URL is null or not
+                Articles newsArticle = widget.newsModel.articles![index]!;
 
-                  // If image URL is not null, show it on list
-                  if(newsArticle.urlToImage != null ||
-                      newsArticle.url != null) {
-                    return NewsItem(newsArticle: newsModel.articles![index]!);
-                  } else { // Else, skip it
-                    print('${newsArticle.title} has null url');
-                    return SizedBox.shrink();
-                  }
-                },
-                itemCount: newsModel.articles!.length,
-              ),
-            ],
-          ),
-        )
+                // If image URL is not null, show it on list
+                if(newsArticle.urlToImage != null ||
+                    newsArticle.url != null) {
+                  return NewsItem(newsArticle: widget.newsModel.articles![index]!);
+                } else { // Else, skip it
+                  print('${newsArticle.title} has null url');
+                  return SizedBox.shrink();
+                }
+              },
+              itemCount: widget.newsModel.articles!.length,
+            ),
+          ],
+        ),
+      )
     );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }

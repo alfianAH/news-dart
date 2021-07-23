7227e0f878707fda02792102f61742a8ad6a1da5
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-07-23 15:13:09 +0800

    Make detail screen be responsive

--- lib/ui/detail_screen.dart
+++ lib/ui/detail_screen.dart
@@ -12,8 +12,6 @@ class DetailScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    TextTheme textTheme = Theme.of(context).textTheme;
-
     return Scaffold(
       appBar: MobileAppBar(
         appBar: AppBar(),
@@ -21,64 +19,166 @@ class DetailScreen extends StatelessWidget{
       ),
 
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // News title
-              Text(
-                newsArticle.title.toString(),
-                style: textTheme.headline2,
-              ),
-
-              SizedBox(height: 8,),
-
-              // News date
-              Text(
-                '${newsArticle.publishedAt.toString()} by ${newsArticle.author.toString()}',
-                style: textTheme.subtitle1
-              ),
-
-              SizedBox(height: 16,),
-
-              // News image
-              Image.network(
-                newsArticle.urlToImage.toString(),
-                errorBuilder: (context, exception, stackTrace){
-                  return Icon(
-                    Icons.broken_image,
-                    color: NewsColors.fontLight,
-                    size: 75,
-                  );
-                },
-              ),
-
-              SizedBox(height: 8,),
-
-              // News link
-              RichText(
-                text: TextSpan(
-                  text: 'Klik untuk melihat artikel asli.',
-                  style: textTheme.caption,
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      launch(newsArticle.url.toString());
-                    }
-                ),
-              ),
-
-              SizedBox(height: 16,),
-
-              // News content
-              Text(
-                newsArticle.content.toString(),
-                style: textTheme.bodyText1,
-              ),
-            ]
-          ),
+        child: LayoutBuilder(
+          builder: (context, constraints){
+            if(constraints.maxWidth > 600){
+              return _DetailWebPage(
+                newsArticle: newsArticle,
+              );
+            } else{
+              return _DetailMobilePage(
+                newsArticle: newsArticle,
+              );
+            }
+          },
         ),
       )
     );
   }
+}
+
+class _DetailMobilePage extends StatelessWidget{
+  final Articles newsArticle;
+
+  _DetailMobilePage({
+    required this.newsArticle
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // News title
+          Text(
+            newsArticle.title.toString(),
+            style: textTheme.headline2,
+          ),
+
+          SizedBox(height: 8,),
+
+          // News date
+          Text(
+            '${newsArticle.publishedAt.toString()} by ${newsArticle.author.toString()}',
+            style: textTheme.subtitle1
+          ),
+
+          SizedBox(height: 16,),
+
+          // News image
+          Image.network(
+            newsArticle.urlToImage.toString(),
+            errorBuilder: (context, exception, stackTrace){
+              return Icon(
+                Icons.broken_image,
+                color: NewsColors.fontLight,
+                size: 75,
+              );
+            },
+          ),
+
+          SizedBox(height: 8,),
+
+          // News link
+          RichText(
+            text: TextSpan(
+              text: 'Klik untuk melihat artikel asli.',
+              style: textTheme.caption,
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  launch(newsArticle.url.toString());
+                }
+            ),
+          ),
+
+          SizedBox(height: 16,),
+
+          // News content
+          Text(
+            newsArticle.content.toString(),
+            style: textTheme.bodyText1,
+          ),
+        ]
+      ),
+    );
+  }
+}
+
+class _DetailWebPage extends StatelessWidget{
+  final Articles newsArticle;
+
+  _DetailWebPage({
+    required this.newsArticle
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 100
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // News title
+          Text(
+            newsArticle.title.toString(),
+            style: textTheme.headline2,
+          ),
+
+          SizedBox(height: 8,),
+
+          // News date
+          Text(
+              '${newsArticle.publishedAt.toString()} by ${newsArticle.author.toString()}',
+              style: textTheme.subtitle1
+          ),
+
+          SizedBox(height: 16,),
+
+          // News image
+          Image.network(
+            newsArticle.urlToImage.toString(),
+            errorBuilder: (context, exception, stackTrace){
+              return Icon(
+                Icons.broken_image,
+                color: NewsColors.fontLight,
+                size: 75,
+              );
+            },
+          ),
+
+          SizedBox(height: 8,),
+
+          // News link
+          RichText(
+            text: TextSpan(
+                text: 'Klik untuk melihat artikel asli.',
+                style: textTheme.caption,
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    launch(newsArticle.url.toString());
+                  }
+            ),
+          ),
+
+          SizedBox(height: 16,),
+
+          // News content
+          Text(
+            newsArticle.content.toString(),
+            style: textTheme.bodyText1,
+          ),
+        ]
+      ),
+    );
+  }
+
 }
